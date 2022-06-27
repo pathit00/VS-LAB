f04c14a3949f94c90b28316b4db74b043a35b4dd
Gohlking <dugoit00@hs-esslingen.de>
2022-06-27 16:38:22 +0200

    funktionen

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ console.log(swaggerDocs);
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'local',
+    password: 'root',
     database: 'verteiltesysteme'
 });
 
@@ -50,8 +50,9 @@ app.get('/', function (request, response) {
 
 // http://localhost:3000/todos/
 
+//http://localhost:3000/api-docs/#/
+
 /**
- * 
  * @swagger
  *   components:
  *      schemas:
@@ -89,7 +90,63 @@ app.get('/', function (request, response) {
  *       400:
  *          description: "Something went wrong"
  * 
+ * /todos/{name}:
+ *  get:
+ *      summary: Get todos with specific name
+ *      parameters:
+ *        - in: path
+ *          name: name
+ *          schema:
+ *              type: string
+ *              required: true
+ *              description: Name of a todo task
+ *      responses:
+ *          200:
+ *              description: "Succesfull response"
+ *          400:
+ *              description: "Something went wrong"
+ * 
+ * /todos/{namedelete}:
+ *  delete:
+ *      summary: Delete todos with specific name
+ *      parameters:
+ *        - in: path
+ *          name: name
+ *          schema:
+ *              type: string
+ *              required: true
+ *              description: Name of a todo task
+ *      responses:
+ *          200:
+ *              description: "Succesfull response"
+ *          400:
+ *              description: "Something went wrong"
+ * 
+ * /todos/{id}/{priority}:
+ *  put:
+ *      summary: update todos with specific id
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: integer
+ *              required: true
+ *              description: id of a todo task
+ *        - in: path
+ *          name: priority
+ *          schema:
+ *              type: integer
+ *              required: true
+ *              description: priority of a todo task
+ *      responses:
+ *          200:
+ *              description: "Succesfull response"
+ *          400:
+ *              description: "Something went wrong"
+ * 
  */
+
+// GET /todos/ (returns a list of todos)
 app.get('/todos', function (request, response) {
     connection.query("SELECT * FROM verteiltesysteme.todos", function (error, results) {
         // If there is an issue with the query, output the error
@@ -97,6 +154,45 @@ app.get('/todos', function (request, response) {
         response.send(results);
     });
 
+});
+
+// GET /todos/{name} (returns a todo if exists)
+app.get('/todos/:name', function(req, res) {
+    connection.query("SELECT * FROM verteiltesysteme.todos WHERE todo = '"+req.params.name + "'", function (error, results) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        res.send(results);
+    });
+  });
+
+//POST /todos/ (send/receive JSON object)
+
+app.post('/todos/', function(req,res){
+
+});
+
+//DELETE  /todos/{name} (deletes a todo if exists)
+app.delete('/todos/:name', function(req,res){
+    connection.query("DELETE FROM verteiltesysteme.todos WHERE todo = '"+req.params.name + "'", function (error, results) {
+        // Query Example: DELETE FROM verteiltesysteme.todos WHERE todo = "hallo"
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        res.send("Successfully deleted todo");
+    });
+});
+
+
+//PUT /todos/ (send/receive JSON object., e.g. can change priority)
+// update todos in database
+// filter by id
+// parameters id of todo that should get updated
+// parameter 2: priority
+app.put('/todos/:id/:priority', function(req,res){
+    // Query Example: UPDATE verteiltesysteme.todos SET priority = 1 WHERE id = 1;
+    connection.query("UPDATE verteiltesysteme.todos SET priority ="+req.params.priority+" WHERE id="+req.params.id, function(error,results){
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
 app.listen(3000);
